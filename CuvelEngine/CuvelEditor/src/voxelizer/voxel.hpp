902838e3d7e6d0b83ad36f8f5cb2e1da6044f5e5
Beni7325 <alejandro1benimeli@gmail.com>
2022-11-06 00:08:20 +0100

    Voxm writer first version

--- CuvelEngine/CuvelEditor/src/voxelizer/voxel.hpp
+++ CuvelEngine/CuvelEditor/src/voxelizer/voxel.hpp
@@ -10,9 +10,10 @@ union VoxelPos
         glm::uint32 x : 10;
         glm::uint32 y : 10;
         glm::uint32 z : 10;
+        glm::uint32 unused : 2;
     };
 
-    VoxelPos(glm::uint32 x, glm::uint32 y, glm::uint32 z) : x(x), y(y), z(z) {};
+    VoxelPos(glm::uint32 x, glm::uint32 y, glm::uint32 z) : x(x), y(y), z(z), unused(0) {};
     VoxelPos(glm::uint32 pos) : pos(pos) {};
 };
 
